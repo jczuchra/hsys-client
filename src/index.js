c8a0cdd52dc5be6648d38cce6efc4811a2a0bfe9
jczuchra <czuchra.jakub@gmail.com>
2020-07-04 15:58:29 +0200

    Add server

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'antd/dist/antd.css';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://hsys-server.herokuapp.com/graphql',
   credentials: 'include',
 });
 
